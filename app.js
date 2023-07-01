f2b4bfbec54bf840fa0966f26e0db20bba3ae54a
Louis Born <louisborn12@gmail.com>
2023-07-01 11:20:51 +0200

    early chat feature

--- app.js
+++ app.js
@@ -3,28 +3,45 @@ require('dotenv').config();
 const express = require('express');
 const app =express();
 const port = 3000
+const {createServer} = require('http');
+const {Server} = require('socket.io');
 
 const mongoose = require('mongoose');
-const http = require('http');
-const io = require('socket.io');
 const bodyParser = require('body-parser');
 
 const repository = require('./repository');
 const userController = require('./user');
-const {Server} = require('socket.io');
 var cors = require('cors');
 const {authenticateToken} = require('./user');
 
-const httpServer = http.createServer(app);
-const server = new Server(httpServer);
+const http = createServer();
+const io = new Server(http);
 mongoose.connect(process.env.db);
 
 app.use(cors());
 app.use(bodyParser());
 
-server.on("connection", (socket) => {
-    console.log("connected");
+io.on("connection", function(socket) {
+
+    socket.on("user_join", function(data) {
+        this.username = data;
+        socket.broadcast.emit("user_join", data);
+    });
+
+    socket.on("chat_message", function(data) {
+        data.username = this.username;
+        socket.broadcast.emit("chat_message", data);
+    });
+
+    socket.on("disconnect", function(data) {
+        socket.broadcast.emit("user_leave", this.username);
+    });
 });
+
+app.get("/chat", function(req, res) {
+    console.log('chatting');
+});
+
 
 app.post('/login', userController.login);
 app.post('/register', userController.register);
@@ -36,6 +53,6 @@ app.post('/match/:acceptor/accept/:sender', authenticateToken, userController.ma
 
 repository.populate();
 
-app.listen(port, () => {
+http.listen(port, () => {
     console.log(`application listening on port: ${port}`);
 });
